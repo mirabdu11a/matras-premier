ffc92119ed372c88eb13ac6528276de99655a5e0
ummataliyev <djohnummataliyev@gmail.com>
2026-05-24 22:46:04 +0500

    feat(products-list): forward categoryId URL param to ProductsCards

--- src/pages/ProductsList.jsx
+++ src/pages/ProductsList.jsx
@@ -1,14 +1,15 @@
-
+import { useParams } from 'react-router-dom'
 import ProductsCards from '../components/ProductsCards'
 import Navbar from '../components/Navbar'
 import Order from '../components/Order'
 
 export default function ProductsList() {
+  const { categoryId } = useParams()
   return (
     <>
-      <Navbar/>
-      <ProductsCards/>
-      <Order/>
+      <Navbar />
+      <ProductsCards categoryId={categoryId} />
+      <Order />
     </>
   )
 }
